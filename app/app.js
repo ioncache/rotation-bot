0bb045dcc6b1203aeb9ca4d4175f8c73aaef1209
Mark Jubenville <ioncache@gmail.com>
2021-05-13 01:30:44 -0400

    ran prettier on initial app.js

--- app/app.js
+++ app/app.js
@@ -1,16 +1,16 @@
 const config = require('config')
-const { App } = require('@slack/bolt');
+const { App } = require('@slack/bolt')
 
 const app = new App({
   signingSecret: config.get('slack.secret'),
   token: config.get('slack.token'),
-});
+})
 
 /* Add functionality here */
 
 (async () => {
   // Start the app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 3000)
 
-  console.log('⚡️ Bolt app is running!');
-})();
+  console.log('⚡️ Bolt app is running!')
+})()
